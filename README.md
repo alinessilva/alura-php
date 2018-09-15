dfbadee4fa3953b22f074dab1ec6f3ee5d9a4548
alinessilva <alinedoze@gmail.com>
2018-09-15 00:16:37 -0300

    Conexão com MySQL

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # alura-php
 Desenvolvimento de uma loja de produtos, desenvolvido com PHP, MySQL e Boostrap.
+
+Para visualizar o projeto, copie e cole o link do index:
+https://htmlpreview.github.io/
--- add-product.php
+++ add-product.php
@@ -3,6 +3,12 @@
 <?php
     $name = $_GET['name'];
     $price = $_GET['price'];
+    $connection = mysqli_connect('localhost', 'root', 'dev@1234', 'loja');
+    // $query = "insert into products (name, price) values ('" . $name . "', " . $price . ")"; concatenação
+    $query = "insert into products (name, price) values ('{$name}', {$price})"; // interpolação
+    
+    mysqli_query($connection, $query);
+    mysqli_close($connection);
 ?>
 
     <p class="alert-sucess">Produto, <?= $name; ?>, <?= $price ?> adicionado com sucesso!</p>
